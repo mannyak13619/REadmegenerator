6ee835c564b1a1de554b5101a75051ccae65379e
manny <emmanuelgomezrealpro@gmail.com>
2023-03-27 09:47:33 -0400

    initial setup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer=require("inquirer")
+const fs=require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["git hub user name?","e-mail address?","Title",""]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +13,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+console.log(hello);
